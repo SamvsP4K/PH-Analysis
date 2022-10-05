01c92d47bea9cd92a08310bdbf06b3258db7c47b
SamvsP4K <soundsofsamvs@gmail.com>
2022-10-05 15:27:22 -0700

    employee retirement info exported

--- queries.sql
+++ queries.sql
@@ -93,4 +93,70 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+DROP TABLE retirement_info;
+
+--Creating a new table for retirement info
+SELECT emp_no,first_name,last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 SELECT * FROM retirement_info;
+
+--Joining departments and dept_managers table
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT * FROM departments;
+
+SELECT retirement_info.emp_no, retirement_info.first_name, retirement_info.last_name, dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT retirement_info.emp_no,retirement_info.first_name,retirement_info.last_name, dept_emp.to_date
+INTO current_emp
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no
+WHERE dept_emp.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+--Employee count by dept number
+SELECT COUNT(current_emp.emp_no), dept_emp.dept_no
+INTO emp_count
+FROM current_emp
+LEFT JOIN dept_emp
+ON current_emp.emp_no = dept_emp.emp_no
+GROUP BY dept_emp.dept_no
+ORDER BY dept_emp.dept_no;
+
+SELECT * FROM current_emp;
+
+--Employee info
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT employees.emp_no,employees.first_name,employees.last_name,employees.gender,salaries.salary,dept_emp.to_date
+INTO emp_detailed_info
+FROM  employees
+INNER JOIN salaries
+on (employees.emp_no = salaries.emp_no)
+INNER JOIN dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (dept_emp.to_date = '9999-01-01');
