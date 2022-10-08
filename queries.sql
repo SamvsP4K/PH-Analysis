113b93e9f6d0d2dc419ea8bd3a9444f26a77aa35
SamvsP4K <soundsofsamvs@gmail.com>
2022-10-08 11:17:45 -0700

    new queries added

--- queries.sql
+++ queries.sql
@@ -160,3 +160,39 @@ ON (employees.emp_no = dept_emp.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 AND (dept_emp.to_date = '9999-01-01');
+
+--Management per department retirement info
+
+SELECT  dept_manager.dept_no,
+        departments.dept_name,
+        dept_manager.emp_no,
+        current_emp.last_name,
+        current_emp.first_name,
+        dept_manager.from_date,
+        dept_manager.to_date
+INTO manager_info
+FROM dept_manager
+INNER JOIN departments
+ON (dept_manager.dept_no = departments.dept_no)
+INNER JOIN current_emp
+ON (dept_manager.emp_no = current_emp.emp_no);
+
+-- department retirees
+SELECT current_emp.emp_no,
+current_emp.first_name,
+current_emp.last_name,
+departments.dept_name
+INTO dept_info
+FROM current_emp
+INNER JOIN dept_emp
+ON (current_emp.emp_no = dept_emp.emp_no)
+INNER JOIN departments
+ON (dept_emp.dept_no = departments.dept_no);
+
+SELECT * FROM dept_emp;
+
+--Sales team info
+SELECT retirement_info.emp_no, retirement_info.first_name,retirement_info.lastname, departments.dept_no, departments.dept_name,dept_emp.emp_no
+FROM retirement_info
+RIGHT JOIN departments
+ON ()
